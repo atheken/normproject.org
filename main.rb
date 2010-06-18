aade11151c0b2e34fa185b0a6339bcb6a0086b6e
Andrew Theken <theken.1@osu.edu>
2010-06-18 18:19:19 -0400

    Added some stubs and routes, not sure I'm doing this right..

--- main.rb
+++ main.rb
@@ -4,5 +4,26 @@ require 'haml'
 set :haml, {:format => :html5 }
 
 get '/' do
-  haml :index
+	haml :index
+end
+get '/documentation'
+	haml :documentation
+end
+get '/samples'
+	haml :samples
+end
+get '/discuss'
+	haml :discuss
+end
+get '/quickstart'
+	haml :quickstart
+end
+get '/contribute'
+	haml :contribute
+end
+get '/contributors'
+	haml :contributoes
+end
+get '*' do
+	haml :status404
 end
